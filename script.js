294f7981e327fea813c251eaa8c14b45e2c9cbb1
MystiaFin <cytrsx01@gmail.com>
2024-11-03 15:49:30 +0700

    update

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ angular
           todos[index] = updatedTodo;
         }
       },
+      deleteTodo: function (id) {
+        var index = todos.findIndex((todo) => todo.id === id);
+        if (index !== -1) {
+          todos.splice(index, 1);
+        }
+      },
       searchTodos: function (searchText) {
         if (!searchText) return todos;
         searchText = searchText.toLowerCase();
@@ -34,6 +40,7 @@ angular
     function ($scope, TodoService) {
       $scope.todos = TodoService.getTodos();
       $scope.searchText = "";
+
       $scope.filterTodos = function () {
         $scope.todos = TodoService.searchTodos($scope.searchText);
       };
@@ -57,36 +64,13 @@ angular
         },
         true
       );
+
+      $scope.deleteTodo = function (id) {
+        TodoService.deleteTodo(id);
+        $scope.todos = TodoService.getTodos();
+      };
     },
   ])
-  .service("TodoService", function () {
-    var todos = [];
-    return {
-      getTodos: function () {
-        return todos;
-      },
-      addTodo: function (todo) {
-        todo.id = todos.length + 1;
-        todos.push(todo);
-      },
-      getTodoById: function (id) {
-        return todos.find((todo) => todo.id === parseInt(id));
-      },
-      updateTodo: function (updatedTodo) {
-        var index = todos.findIndex((todo) => todo.id === updatedTodo.id);
-        if (index !== -1) {
-          todos[index] = updatedTodo;
-        }
-      },
-      searchTodos: function (searchText) {
-        if (!searchText) return todos;
-        searchText = searchText.toLowerCase();
-        return todos.filter((todo) =>
-          todo.title.toLowerCase().includes(searchText)
-        );
-      },
-    };
-  })
   .controller("NewListController", [
     "$scope",
     "TodoService",
@@ -115,6 +99,28 @@ angular
       $scope.saveTodo = function () {
         TodoService.updateTodo($scope.todo);
         $location.path("/");
+      };
+    },
+  ])
+  .controller("SidebarController", [
+    "$scope",
+    "TodoService",
+    function ($scope, TodoService) {
+      $scope.categories = [
+        { name: "important", visible: true },
+        { name: "jobs", visible: true },
+        { name: "other", visible: true },
+      ];
+
+      $scope.todos = TodoService.getTodos();
+
+      $scope.toggleSidebar = function () {
+        const sidebar = document.getElementById("sidebar");
+        sidebar.classList.toggle("open");
+      };
+
+      $scope.toggleCategory = function (category) {
+        category.visible = !category.visible;
       };
     },
   ]);
